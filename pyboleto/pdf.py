3233f58010e287a83f46752007459974b9c96837
diegojsanches <diegojsanches@hotmail.com>
2018-02-07 18:29:52 -0200

    Correção das margem na impressão de carne
    Ao imprimir o carne o logotipo do banco e a linha digitavel saem fora da pagina

--- pyboleto/pdf.py
+++ pyboleto/pdf.py
@@ -776,11 +776,11 @@ class BoletoPDF(object):
         :type boletoDados2: :class:`pyboleto.data.BoletoData`
 
         """
-        y = 5 * mm
+        y = 3 * mm
         d = self.drawBoletoCarne(boletoDados1, y)
         y += d[1] + 6 * mm
         # self._drawHorizontalCorteLine(0, y, d[0])
-        y += 7 * mm
+        y += 2 * mm
         if boletoDados2:
             self.drawBoletoCarne(boletoDados2, y)
 
